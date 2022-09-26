dacf9eb8edecc864327c7ced9ebbce41fe713e56
veropov <108414036+veropov@users.noreply.github.com>
2022-09-26 16:57:31 +0800

    final layout third Login page

--- lib/pages/Sign up/Login_thirdPage.dart
+++ lib/pages/Sign up/Login_thirdPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:aga/constant.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class LoginThirdPage extends StatefulWidget {
   const LoginThirdPage({Key? key}) : super(key: key);
@@ -10,10 +11,47 @@ class LoginThirdPage extends StatefulWidget {
 }
 
 class _LoginThirdPageState extends State<LoginThirdPage> {
+
+
+
   @override
   Widget build(BuildContext context) {
+
     final heightScreen = MediaQuery.of(context).size.height;
     final widthScreen = MediaQuery.of(context).size.width;
+
+    final pictures = [
+      'assets/images/Login_third_page.svg', 
+      'assets/images/girl_fivePage.svg', 
+      'assets/images/Login_third_page.svg'
+    ];
+
+    Widget buildImage(dynamic picture, int index) => Stack(
+      alignment: Alignment.center,
+      children: [
+      Container(
+        decoration: BoxDecoration(gradient: LinearGradient(
+          colors: [Color.fromARGB(255, 154, 195, 254), Color.fromARGB(255, 149, 174, 254)], 
+            begin: Alignment.topLeft, 
+            end: Alignment.bottomRight
+        ),
+        borderRadius: BorderRadius.circular(22),
+        ),
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          SvgPicture.asset(picture, height: heightScreen*0.36, fit: BoxFit.cover),
+          Column(children: [
+            Text('Improve Shape', style: TextStyle(fontSize: 14, height: 1.5, fontWeight: FontWeight.w600, color: Colors.white, fontFamily: "Poppins")),
+            SizedBox(height: 20),
+            Text('I have a low amount of body fat\n and need / want to build more\n muscle', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, color: Colors.white, fontFamily: "Poppins"), textAlign: TextAlign.center,)
+          ]),
+        ],
+      )
+    ],
+  );
+    
     return SafeArea(child: 
       Scaffold(
         body: Column(
@@ -29,34 +67,17 @@ class _LoginThirdPageState extends State<LoginThirdPage> {
                 Text('It will help us to choose a best\n program for you', style: kTextMain, textAlign: TextAlign.center)
               ],)
             ),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  decoration: BoxDecoration(gradient: LinearGradient(
-                    colors: [Color.fromARGB(255, 154, 195, 254), Color.fromARGB(255, 149, 174, 254)], 
-                    begin: Alignment.topLeft, 
-                    end: Alignment.bottomRight
-                    ),
-                    borderRadius: BorderRadius.circular(22),
-                  ),
-                  width: widthScreen*0.73,
-                  height: heightScreen * 0.59
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                    SvgPicture.asset('assets/images/Login_third_page.svg', height: heightScreen*0.36),
-                    Column(children: [
-                      SizedBox(height: 20),
-                      Text('Improve Shape', style: TextStyle(fontSize: 14, height: 1.5, fontWeight: FontWeight.w600, color: Colors.white, fontFamily: "Poppins")),
-                      Text('I have a low amount of body fat\n and need / want to build more\n muscle', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, color: Colors.white, fontFamily: "Poppins"), textAlign: TextAlign.center,)
-                    ],)
-                  ],)
-                )
-              ],
+            CarouselSlider.builder(
+              options: CarouselOptions(
+                height: heightScreen*0.59,
+                enlargeCenterPage: true
+              ),
+              itemCount: pictures.length,
+              itemBuilder: (context, index, realIndex) {
+
+                final picture = pictures[index];
+                return buildImage(picture, index);
+              },
             ),
             Container(
               height: 60,
@@ -92,3 +113,7 @@ class _LoginThirdPageState extends State<LoginThirdPage> {
     );
   }
 }
+
+
+
+
