860d9ae0b820a923c3dfeb394ae14f8a9b2fc06f
veropov <108414036+veropov@users.noreply.github.com>
2022-09-28 12:42:07 +0800

    change pages

---
+++ lib/pages/Sign up/Login_welcome.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:aga/constant.dart';
+
+class LoginWelcome extends StatelessWidget {
+  const LoginWelcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: 
+    Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          
+      ],)
+      )
+    );
+  }
+}
