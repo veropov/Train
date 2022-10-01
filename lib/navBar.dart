8235b59efb4dfa14e9bc262746c3a08e1fc7cf25
veropov <108414036+veropov@users.noreply.github.com>
2022-10-02 04:20:30 +0800

    refresh bottom navigation + add float button

--- lib/navBar.dart
+++ lib/navBar.dart
@@ -14,42 +14,50 @@ class _NavigatBarState extends State<NavigatBar> {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      currentIndex: currentIndex,
-      onTap: (index) => setState(() => currentIndex = index),
-      elevation: 2,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      items: [
-        BottomNavigationBarItem(
-          label: '',
-          icon: currentIndex == 0 ? SvgPicture.asset('assets/icons/Home_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Home_light.svg', height: 24, color: kGray100)
-        ),
-        BottomNavigationBarItem(
-          label: '',
-          icon: currentIndex == 1 ? SvgPicture.asset('assets/icons/Activity_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Activity_light.svg', height: 24, color: kGray100)
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('assets/icons/Activity_fill.svg', height: 1, color: Colors.transparent),
-          label: '',
-        ),
-        BottomNavigationBarItem(
-          label: '',
-          icon: currentIndex == 3 ? SvgPicture.asset('assets/icons/Camera_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Camera_light.svg', height: 24, color: kGray100)
-        ),
-        BottomNavigationBarItem(
-          label: '',
-          icon: currentIndex == 4 ? SvgPicture.asset('assets/icons/Profile_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Profile_light.svg', height: 24, color: kGray100)
-        )
-      ]
+    return NavigationBarTheme(
+      data: NavigationBarThemeData(
+        elevation: 1,
+        height: 65,
+        backgroundColor: Colors.white,
+        indicatorColor: Colors.transparent,
+        indicatorShape: StadiumBorder()
+      ),
+      child: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        height: 60,
+        selectedIndex: currentIndex,
+        onDestinationSelected: (index) => setState(() => currentIndex = index),
+        destinations: [
+          NavigationDestination(
+            label: '',
+            icon: currentIndex == 0 ? SvgPicture.asset('assets/icons/Home_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Home_light.svg', height: 24, color: kGray100)
+          ),
+          NavigationDestination(
+            label: '',
+            icon: currentIndex == 1 ? SvgPicture.asset('assets/icons/Activity_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Activity_light.svg', height: 24, color: kGray100)
+          ),
+          SizedBox(width: 25),
+          NavigationDestination(
+            label: '',
+            icon: currentIndex == 3 ? SvgPicture.asset('assets/icons/Camera_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Camera_light.svg', height: 24, color: kGray100)
+          ),
+          NavigationDestination(
+            label: '',
+            icon: currentIndex == 4 ? SvgPicture.asset('assets/icons/Profile_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Profile_light.svg', height: 24, color: kGray100)
+          )
+      ],)
     );
   }
 }
 
-Widget FloatButton() => FloatingActionButton(
+Widget FloatButton() => SizedBox.fromSize(
+  size: Size.square(60),
+  child: FloatingActionButton(
   child: SvgPicture.asset('assets/icons/Search_light.svg', height: 20, color: Colors.white),
   elevation: 2,
+  highlightElevation: 2,
+  splashColor: Color.fromARGB(255, 56, 126, 255),
   backgroundColor: Color.fromARGB(255, 149, 174, 254),
   onPressed: () {},
-  );
+  )
+);
