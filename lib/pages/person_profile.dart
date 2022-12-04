6545ef9df3aadaafa606aec7aa97392264569691
veropov <108414036+veropov@users.noreply.github.com>
2022-12-04 21:38:04 +0800

    Update person_profile.dart

--- lib/pages/person_profile.dart
+++ lib/pages/person_profile.dart
@@ -192,97 +192,226 @@ Widget Avatar() => SingleChildScrollView(child:
           ],
         ),
       ),
-      Account()
+      Account(),
+      Notification(),
+      Other()
     ],
   ),
 );
 
 Widget Account() => Container(
-  margin: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
+  margin: EdgeInsets.only(left: 30, right: 30, top: 25, bottom: 15),
   decoration: BoxDecoration(
     borderRadius: BorderRadius.circular(16),
-    color: Colors.white,
+    color: const Color.fromARGB(200, 255, 255, 255),
     boxShadow: [
         const BoxShadow(
-          color: Color.fromARGB(10, 15, 5, 5),
+          color: Color.fromARGB(15, 15, 5, 5),
           blurRadius: 30,
         )
       ] 
   ),
-  child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Container(
-        margin: EdgeInsets.only(left: 20, top: 15),
-        child: Text('Account', style: TextStyle(fontSize: 16, height: 1.5, fontWeight: FontWeight.w600, fontFamily: "Poppins"))
-      ),
-      Container(
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
+  child: InkWell(
+    borderRadius: BorderRadius.circular(16),
+    onTap: () {},
+    highlightColor: const Color.fromARGB(255, 255, 255, 255),
+    splashColor: const Color.fromARGB(255, 118, 103, 255),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.only(left: 20, top: 15),
+          child: Text('Account', style: TextStyle(fontSize: 16, height: 1.5, fontWeight: FontWeight.w600, fontFamily: "Poppins"))
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(width: 2),
+                      SvgPicture.asset('assets/icons/Profile_light.svg', color: Color.fromARGB(255, 126, 145, 253), height: 16),
+                      SizedBox(width: 16),
+                      Text('Personal Data', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
+                    ],
+                  ),
+                  SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SvgPicture.asset('assets/icons/Document_light.svg', color: Color.fromARGB(255, 126, 145, 253), height: 16),
+                      SizedBox(width: 15),
+                      Text('Achievement', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
+                    ],
+                  ),
+                  SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SvgPicture.asset('assets/icons/Graph_light.svg', color: Color.fromARGB(255, 126, 145, 253), height: 16),
+                      SizedBox(width: 15),
+                      Text('Activity History', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
+                    ],
+                  ),
+                  SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SvgPicture.asset('assets/icons/Chart_light.svg', color: Color.fromARGB(255, 126, 145, 253), height: 16),
+                      SizedBox(width: 15),
+                      Text('Workout Progress', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
+                    ],
+                  ),
+                  SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  ),
+);
+
+Widget Notification() => Container(
+  margin: EdgeInsets.symmetric(horizontal: 30),
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(16),
+    color: const Color.fromARGB(200, 255, 255, 255),
+    boxShadow: [
+        const BoxShadow(
+          color: Color.fromARGB(15, 15, 5, 5),
+          blurRadius: 30,
+        )
+      ] 
+  ),
+  child: Container(
+    margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Notification', style: TextStyle(fontSize: 16, height: 1.5, fontWeight: FontWeight.w600, fontFamily: "Poppins")),
+        SizedBox(height: 15),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
-                  children: [
-                    SvgPicture.asset('assets/icons/Profile_light.svg', color: Colors.blue, height: 16),
-                    SizedBox(width: 15),
-                    Text('Personal Data', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
-                  ],
-                ),
-                SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
+                SvgPicture.asset('assets/icons/Notification_light.svg', color: Color.fromARGB(255, 126, 145, 253), height: 16),
+                SizedBox(width: 15),
+                Text('Pop-up Notification', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
               ],
             ),
-            SizedBox(height: 15),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    SvgPicture.asset('assets/icons/Profile_light.svg', color: Colors.blue, height: 16),
-                    SizedBox(width: 15),
-                    Text('Achievement', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
-                  ],
-                ),
-                SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
-              ],
-            ),
-            SizedBox(height: 15),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    SvgPicture.asset('assets/icons/Profile_light.svg', color: Colors.blue, height: 16),
-                    SizedBox(width: 15),
-                    Text('Activity History', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
-                  ],
-                ),
-                SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
-              ],
-            ),
-            SizedBox(height: 15),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    SvgPicture.asset('assets/icons/Profile_light.svg', color: Colors.blue, height: 16),
-                    SizedBox(width: 15),
-                    Text('Workout Progress', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
-                  ],
-                ),
-                SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
-              ],
-            ),
+            SwitchButton()
           ],
         ),
-      ),
-    ],
+      ],
+    ),
+  ),
+);
+
+Widget Other() => Container(
+  margin: EdgeInsets.only(left: 30, right: 30, top: 15, bottom: 30),
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(16),
+    color: const Color.fromARGB(200, 255, 255, 255),
+    boxShadow: [
+        const BoxShadow(
+          color: Color.fromARGB(15, 15, 5, 5),
+          blurRadius: 30,
+        )
+      ] 
+  ),
+  child: InkWell(
+    borderRadius: BorderRadius.circular(16),
+    onTap: () {},
+    highlightColor: const Color.fromARGB(255, 255, 255, 255),
+    splashColor: const Color.fromARGB(255, 118, 103, 255),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.only(left: 20, top: 15),
+          child: Text('Other', style: TextStyle(fontSize: 16, height: 1.5, fontWeight: FontWeight.w600, fontFamily: "Poppins"))
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SvgPicture.asset('assets/icons/Message_light.svg', color: Color.fromARGB(255, 126, 145, 253), height: 16),
+                      SizedBox(width: 15),
+                      Text('Contact Us', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
+                    ],
+                  ),
+                  SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SvgPicture.asset('assets/icons/Shield Done_light.svg', color: Color.fromARGB(255, 126, 145, 253), height: 16),
+                      SizedBox(width: 15),
+                      Text('Privacy Policy', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
+                    ],
+                  ),
+                  SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      SvgPicture.asset('assets/icons/Setting_light.svg', color: Color.fromARGB(255, 126, 145, 253), height: 16),
+                      SizedBox(width: 15),
+                      Text('Settings', style: TextStyle(fontSize: 12, height: 1.5, fontWeight: FontWeight.w400, fontFamily: "Poppins", color: kGray100)),
+                    ],
+                  ),
+                  SvgPicture.asset('assets/icons/Arrow - Right 2_light.svg', color: kGray100, height: 20)
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
   ),
 );
