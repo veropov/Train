5388508df6f403e0c06229b7b2abdb1a5bce6fe8
veropov <108414036+veropov@users.noreply.github.com>
2022-12-17 23:45:14 +0800

    Update Activity_traker.dart

--- lib/pages/DashBoard/Activity traker/Activity_traker.dart
+++ lib/pages/DashBoard/Activity traker/Activity_traker.dart
@@ -3,9 +3,10 @@ import 'package:aga/constant.dart';
 import '../FirstPage/DashBoard_firstPage.dart';
 import 'package:aga/transition.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:aga/navBar.dart';
 import 'package:aga/pages/DashBoard/Activity traker/List_target.dart';
 import 'package:aga/pages/DashBoard/Activity traker/Activity_progress.dart';
+import 'package:aga/pages/DashBoard/FirstPage/HomePage.dart';
+import 'package:aga/pages/Profile/person_profile.dart';
 
 class Activity extends StatefulWidget {
   const Activity({Key? key}) : super(key: key);
@@ -16,48 +17,116 @@ class Activity extends StatefulWidget {
 
 class _ActivityState extends State<Activity> {
 
+  Widget AppTrack(context) => AppBar(
+    toolbarHeight: 60,
+    title: Text('Body mass index', style: const TextStyle(fontSize: 14, height: 1.5, fontWeight: FontWeight.w600, fontFamily: "Poppins", color: Color.fromARGB(255, 44, 44, 44))),
+    titleTextStyle: TextStyle(),
+    elevation: 0,
+    backgroundColor: Color.fromARGB(220, 255, 255, 255),
+    leading: Stack(
+      alignment: Alignment.center,
+      children: [Container(
+        margin: const EdgeInsets.only(left: 20),
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(colors: [Color.fromARGB(121, 238, 238, 238), Color.fromARGB(134, 238, 233, 255)], 
+            begin: Alignment.topCenter, 
+            end: Alignment.bottomCenter
+          ),
+          borderRadius: BorderRadius.circular(8)
+        )
+      ),
+      Container(
+        margin: const EdgeInsets.only(left: 20),
+        child: IconButton(
+          icon: SvgPicture.asset('assets/icons/Arrow - Left_light.svg', height: 16),
+          splashRadius: 16,
+          onPressed: () {
+            Navigator.pop(context);
+          }
+        )
+      )
+    ],)
+);
+
   var count = 1;
   bool countElem = false;
 
+    void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+    widgetList() {
+    if (currentIndex == 0) {return Tracker();}
+    if (currentIndex == 1) {return Profile();}
+    if (currentIndex == 3) {return Home();}
+    if (currentIndex == 4) {return Profile();}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: currentIndex == 0 ? Size.fromHeight(60) : Size.fromHeight(0),
+          child: currentIndex == 0 ? AppTrack(context) : Container()),
+      body: widgetList(),
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          elevation: 0,
+          height: 65,
+          backgroundColor: Colors.white,
+          indicatorColor: Colors.transparent,
+          indicatorShape: StadiumBorder()
+        ),
+        child: NavigationBar(
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+          height: 60,
+          selectedIndex: currentIndex,
+          onDestinationSelected: onTap,
+          destinations: [
+            NavigationDestination(
+              label: '',
+              icon: currentIndex == 0 ? SvgPicture.asset('assets/icons/Home_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Home_light.svg', height: 24, color: kGray100)
+            ),
+            NavigationDestination(
+              label: '',
+              icon: currentIndex == 1 ? SvgPicture.asset('assets/icons/Activity_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Activity_light.svg', height: 24, color: kGray100)
+            ),
+            SizedBox(width: 25),
+            NavigationDestination(
+              label: '',
+              icon: currentIndex == 3 ? SvgPicture.asset('assets/icons/Camera_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Camera_light.svg', height: 24, color: kGray100)
+            ),
+            NavigationDestination(
+              label: '',
+              icon: currentIndex == 4 ? SvgPicture.asset('assets/icons/Profile_fill.svg', height: 26, color: shadowPurple) : SvgPicture.asset('assets/icons/Profile_light.svg', height: 24, color: kGray100)
+            )
+        ],)
+      ),
+      floatingActionButton: FloatButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterDocked,
+    );
+  }
+}
+
+class Tracker extends StatefulWidget {
+  const Tracker({Key? key}) : super(key: key);
+
+  @override
+  State<Tracker> createState() => _TrackerState();
+}
+
+class _TrackerState extends State<Tracker> {
   @override
   Widget build(BuildContext context) {
 
     final widthScreen = MediaQuery.of(context).size.width;
 
-    return Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 60,
-          title: Text('Body mass index', style: const TextStyle(fontSize: 14, height: 1.5, fontWeight: FontWeight.w600, fontFamily: "Poppins", color: Color.fromARGB(255, 44, 44, 44))),
-          titleTextStyle: TextStyle(),
-          elevation: 0,
-          backgroundColor: Color.fromARGB(220, 255, 255, 255),
-          leading: Stack(
-            alignment: Alignment.center,
-            children: [Container(
-              margin: const EdgeInsets.only(left: 20),
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(colors: [Color.fromARGB(121, 238, 238, 238), Color.fromARGB(134, 238, 233, 255)], 
-                  begin: Alignment.topCenter, 
-                  end: Alignment.bottomCenter
-                ),
-                borderRadius: BorderRadius.circular(8)
-              )
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 20),
-              child: IconButton(
-                icon: SvgPicture.asset('assets/icons/Arrow - Left_light.svg', height: 16),
-                splashRadius: 16,
-                onPressed: () {
-                  Navigator.pop(context);
-                }
-              )
-            )
-          ],)
-      ),
-      body: Column(
+    return Column(
         children: [
           Container(
             margin: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
@@ -117,8 +186,7 @@ class _ActivityState extends State<Activity> {
           ),
           ActProgress()
         ],
-      )
-    );
+      );
   }
 }
 
